3a9e6b2140b86bb9fb837cff760f53689d5881e0
NevenaKoynarcheva <nevena19k@gmail.com>
2025-02-13 20:50:20 +0200

    Variables on the models

--- src/main/java/com/hotel/HotelService/model/Room.java
+++ src/main/java/com/hotel/HotelService/model/Room.java
@@ -1,4 +1,71 @@
 package com.hotel.HotelService.model;
 
+import java.util.List;
+
 public class Room {
+    private int id;
+    private int roomNumber;
+    private String roomType;
+    private double price;
+    private List<String> benefits;
+    private String bookingStatus;
+
+    public Room(int id, int roomNumber, String roomType, double price, List<String> benefits, String bookingStatus) {
+        this.id = id;
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.price = price;
+        this.benefits = benefits;
+        this.bookingStatus = bookingStatus;
+    }
+
+    public Room(){};
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public List<String> getBenefits() {
+        return benefits;
+    }
+
+    public void setBenefits(List<String> benefits) {
+        this.benefits = benefits;
+    }
+
+    public String getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setBookingStatus(String bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
 }
