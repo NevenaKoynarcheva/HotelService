1febc22ebc60022d3b25aae19433823c82d9f201
NevenaKoynarcheva <nevena19k@gmail.com>
2025-02-24 16:34:27 +0200

    Add admin log in logic

--- src/main/java/com/hotel/HotelService/repository/AdminRepository.java
+++ src/main/java/com/hotel/HotelService/repository/AdminRepository.java
@@ -1,9 +1,14 @@
 package com.hotel.HotelService.repository;
 import com.hotel.HotelService.model.Admin;
 
+import com.hotel.HotelService.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Integer> {
+    boolean existsByUsername(String username);
+    Optional<Admin> findByUsername(String username);
 }
--- src/main/java/com/hotel/HotelService/service/AdminService.java
+++ src/main/java/com/hotel/HotelService/service/AdminService.java
@@ -1,15 +1,32 @@
 package com.hotel.HotelService.service;
 
+import com.hotel.HotelService.model.Admin;
 import com.hotel.HotelService.model.Room;
+import com.hotel.HotelService.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
     @Autowired
-    private AdminService adminService;
+    private AdminRepository adminRepository;
     @Autowired
     private RoomService roomService;
+    private Admin getAdmin(String username) {
+        return adminRepository.findByUsername(username).orElse(null);
+    }
+    public void logIn(String username, String password) {
+        if (!adminRepository.existsByUsername(username)) {
+            throw new RuntimeException("Некоректни данни");
+        } else {
+            Admin admin = getAdmin(username);
+            if (admin != null) {
+                if (!admin.getPassword().equals(password)) {
+                    throw new RuntimeException("Некоректни данни");
+                }
+            }
+        }
+    }
 
     public void addRoom(Room room) {
       roomService.addRoom(room);
